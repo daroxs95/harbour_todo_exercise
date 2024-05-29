290e2b8426422df2875dfd3c1f737d5c11f899b7
Darián López Utra <daroxs95@gmail.com>
2024-05-29 11:25:41 +0200

    Add add todo mutation

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,9 +1,11 @@
-"use client";
+'use client';
 
 import { useState } from 'react';
 import { Heart } from '@/components/icons/Heart';
 import { Close } from '@/components/icons/Close';
 import { AddTodo } from '@/components/AddTodo';
+import { gql } from 'graphql-request';
+import { client } from '@/lib/client';
 
 export type Todo = {
   id: number;
@@ -16,11 +18,22 @@ type TodosProps = {
   list: Todo[];
 };
 
+const ADD_TODO_MUTATION = gql`
+    mutation AddTODO($listId: Int!, $desc: String!) {
+        addTODO(listId: $listId, desc: $desc) {
+            desc
+        }
+    }
+`;
+
 export const Todos = ({ list = [], listId }: TodosProps) => {
   const [todos, setTodos] = useState<Todo[]>(list);
 
   const onAddHandler = (desc: string) => {
-    console.log(`Add todo ${desc}`);
+    return client.request(ADD_TODO_MUTATION, {
+      listId,
+      desc,
+    });
   };
 
   const onRemoveHandler = (id: number) => {
